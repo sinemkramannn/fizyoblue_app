a4d238184a52b88afbead0dd61f3c35c580a5904
Sinem Karaman <sinemkaramannn@gmail.comm>
2024-09-22 23:04:44 +0300

    Project Init

---
+++ src/main/java/com/fizyoblue_app/fizyoblue_app/FizyoblueAppApplication.java
@@ -0,0 +1,13 @@
+package com.fizyoblue_app.fizyoblue_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizyoblueAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizyoblueAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fizyoblue_app/fizyoblue_app/FizyoblueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fizyoblue_app.fizyoblue_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizyoblueAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
